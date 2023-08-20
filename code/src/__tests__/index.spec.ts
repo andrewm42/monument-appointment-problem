d6c02f44ea9aa36799da6d432fea1ff0a326b3fa
Andrew Martin <andrew.martin@apiture.com>
2023-08-20 14:32:23 -0400

    Refactor to latest requirements

--- code/src/__tests__/index.spec.ts
+++ code/src/__tests__/index.spec.ts
@@ -1,98 +1,152 @@
-import { validateSlots } from '../index';
 import moment from 'moment-timezone';
+import { validateSlots, isSlotWithinRange, parseHourAndMinute, RequestedAppointment, Slot } from '../index';
 
-describe('validateSlots function', () => {
+const slots = [
+  {
+    key: '815007',
+    text: '06:00 - 08:00 (1)',
+    value: '815007',
+    ContainerNo: 'TGBU5454043',
+    date: '07/19/2023'
+  },
+  {
+    key: '815008',
+    text: '08:00 - 10:00 (5)',
+    value: '815008',
+    ContainerNo: 'TGBU5454043',
+    date: '07/19/2023'
+  },
+  {
+    key: '815009',
+    text: '10:00 - 12:00 (12)',
+    value: '815009',
+    ContainerNo: 'TGBU5454043',
+    date: '07/19/2023'
+  },
+  {
+    key: '815010',
+    text: '12:00 - 14:00 (12)',
+    value: '815010',
+    ContainerNo: 'TGBU5454043',
+    date: '07/19/2023'
+  },
+  {
+    key: '815011',
+    text: '14:00 - 16:00 (9)',
+    value: '815011',
+    ContainerNo: 'TGBU5454043',
+    date: '07/19/2023'
+  },
+  {
+    key: '815025',
+    text: '06:00 - 08:00 (9)',
+    value: '815025',
+    ContainerNo: 'TGBU5454043',
+    date: '07/20/2023'
+  },
+  {
+    key: '815026',
+    text: '08:00 - 10:00 (17)',
+    value: '815026',
+    ContainerNo: 'TGBU5454043',
+    date: '07/20/2023'
+  },
+  {
+    key: '815027',
+    text: '10:00 - 13:00 (16)',
+    value: '815027',
+    ContainerNo: 'TGBU5454043',
+    date: '07/20/2023'
+  },
+  {
+    key: '815028',
+    text: '12:00 - 14:00 (15)',
+    value: '815028',
+    ContainerNo: 'TGBU5454043',
+    date: '07/20/2023'
+  },
+  {
+    key: '815029',
+    text: '09:00 - 15:00 (15)',
+    value: '815029',
+    ContainerNo: 'TGBU5454043',
+    date: '07/20/2023'
+  }
+];
+
+describe('validateSlots', () => {
   const userTimezone = 'America/New_York';
+  const currentTime = "Thu Jul 20 2023 06:05:00 GMT-0600";
+  const lastFreeDate = "Thu Jul 20 2023 15:09:00 GMT-0600";
 
-  test('should return available slots correctly', () => {
-    const currentTime = "2023-07-20T08:05:00-06:00";
-    const lastFreeDate = "2023-07-20T11:09:00-06:00";
-    const bookedAppointmentTimes = [
-      { appointmentTimeStart: '2023-07-20T01:00:00-06:00', appointmentTimeEnd: '2023-07-20T11:00:00-06:00' },
-      { appointmentTimeStart: '2023-07-21T13:00:00-04:00', appointmentTimeEnd: '2023-07-21T15:00:00-04:00' }
-    ];
-    const slots = [
-      {
-        key: '815007',
-        text: '06:00 - 08:00 (1)',
-        value: '815007',
-        ContainerNo: 'TGBU5454043',
-        date: '07/19/2023'
-      },
-      {
-        key: '815008',
-        text: '08:00 - 10:00 (5)',
-        value: '815008',
-        ContainerNo: 'TGBU5454043',
-        date: '07/19/2023'
-      },
-      {
-        key: '815009',
-        text: '10:00 - 12:00 (12)',
-        value: '815009',
-        ContainerNo: 'TGBU5454043',
-        date: '07/19/2023'
-      },
-      {
-        key: '815010',
-        text: '12:00 - 14:00 (12)',
-        value: '815010',
-        ContainerNo: 'TGBU5454043',
-        date: '07/19/2023'
-      },
-      {
-        key: '815011',
-        text: '14:00 - 16:00 (9)',
-        value: '815011',
-        ContainerNo: 'TGBU5454043',
-        date: '07/19/2023'
-      },
-      {
-        key: '815025',
-        text: '06:00 - 08:00 (9)',
-        value: '815025',
-        ContainerNo: 'TGBU5454043',
-        date: '07/20/2023'
-      },
-      {
-        key: '815026',
-        text: '08:00 - 10:00 (17)',
-        value: '815026',
-        ContainerNo: 'TGBU5454043',
-        date: '07/20/2023'
-      },
-      {
-        key: '815027',
-        text: '10:00 - 12:00 (16)',
-        value: '815027',
-        ContainerNo: 'TGBU5454043',
-        date: '07/20/2023'
-      },
-      {
-        key: '815028',
-        text: '12:00 - 14:00 (15)',
-        value: '815028',
-        ContainerNo: 'TGBU5454043',
-        date: '07/20/2023'
-      },
-      {
-        key: '815029',
-        text: '13:00 - 15:00 (15)',
-        value: '815029',
-        ContainerNo: 'TGBU5454043',
-        date: '07/20/2023'
-      }
-    ];
-
-    const result = validateSlots(userTimezone, new Date(currentTime), new Date(lastFreeDate), bookedAppointmentTimes, slots);
-
-    const expected = [
-      [new Date("2023-07-20T13:00:00-04:00"), new Date("2023-07-20T15:00:00-04:00")]
-    ];
-
-    expect(result).toEqual(expected);
+  test('should return available slots for a time range', () => {
+    const requestedTimes: RequestedAppointment[] = [{ appointmentTimeStart: '0100', appointmentTimeEnd: '2100' }];
+    const availableSlots = validateSlots(userTimezone, currentTime, lastFreeDate, requestedTimes, slots);
+    expect(availableSlots).toEqual(
+      [
+        [
+          new Date("2023-07-20T14:00:00.000Z"),
+          new Date("2023-07-20T17:00:00.000Z"),
+        ],
+        [
+          new Date("2023-07-20T16:00:00.000Z"),
+          new Date("2023-07-20T18:00:00.000Z"),
+        ],
+      ],
+    );
   });
 
-  // More test cases can be added here to test various scenarios.
+  test('should return available slots for a specific time', () => {
+    const requestedTimes: RequestedAppointment[] = [{ appointmentTimeStart: '2023-07-20T06:05:49-06:00', appointmentTimeEnd: '2023-07-20T07:50:49-06:00' }];
+    const availableSlots = validateSlots(userTimezone, currentTime, lastFreeDate, requestedTimes, slots);
+
+    expect(availableSlots).toEqual(
+      [
+        [
+          new Date("2023-07-20T12:00:00.000Z"),
+          new Date("2023-07-20T14:00:00.000Z")
+        ],
+      ],
+    );
+  });
 });
 
+describe('isSlotWithinRange', () => {
+  test('should return true if slot is within range', () => {
+    const slot = {
+      start: moment.tz('07/20/2023 08:00', 'MM/DD/YYYY HH:mm', 'America/New_York'),
+      end: moment.tz('07/20/2023 10:00', 'MM/DD/YYYY HH:mm', 'America/New_York')
+    };
+    const requestedTime: RequestedAppointment = {
+      appointmentTimeStart: '0700',
+      appointmentTimeEnd: '1200',
+    };
+
+    const result = isSlotWithinRange(slot, requestedTime);
+
+    expect(result).toBe(true);
+  });
+
+  test('should return false for a slot outside the time range', () => {
+    const slot: Slot = {
+      start: moment.tz("2023-07-20T22:00:00.000Z", 'MM/DD/YYYY HH:mm', 'America/New_York'),
+      end: moment.tz("2023-07-20T23:00:00.000Z", 'MM/DD/YYYY HH:mm', 'America/New_York')
+    };
+
+    const requestedTime: RequestedAppointment = {
+      appointmentTimeStart: '0100',
+      appointmentTimeEnd: '2100',
+    };
+
+    const result = isSlotWithinRange(slot, requestedTime);
+
+    expect(result).toBe(false);
+  });
+});
+
+describe('extractHourAndMinute', () => {
+  test('should correctly extract hour and minute from military time format', () => {
+    const timeStr = '0130';
+    expect(parseHourAndMinute(timeStr)).toEqual([1, 30]);
+  });
+});
--- code/src/index.ts
+++ code/src/index.ts
@@ -1,65 +1,95 @@
-import moment, { Moment } from 'moment-timezone';
+import moment from 'moment-timezone';
 
-interface BookedAppointments {
+export interface RequestedAppointment {
     appointmentTimeStart: string;
     appointmentTimeEnd: string;
 }
 
+export interface Slot {
+    start: moment.Moment,
+    end: moment.Moment,
+}
+
 export function validateSlots(
     userTimezone: string,
-    currentTime: Date,
-    lastFreeDate: Date,
-    bookedAppointmentTimes: BookedAppointments[],
+    currentTime: string,
+    lastFreeDate: string,
+    requestedAppointmentTimes: RequestedAppointment[],
     slots: any[]
 ): [Date, Date][] {
+    const currentMoment = moment(new Date(currentTime).toISOString()).tz(userTimezone);
+    const lastFreeMoment = moment(new Date(lastFreeDate).toISOString()).tz(userTimezone);
 
-    let currentMoment = moment.tz(currentTime, userTimezone).add(1, 'hours');
-    let lastFreeMoment = moment.tz(lastFreeDate, userTimezone);
+    let results: [Date, Date][] = [];
 
-    let availableSlots: [Date, Date][] = [];
+    for (let s of slots) {
+        const slot: Slot = parseSlot(s, userTimezone);
 
-    for (let slot of slots) {
-        let [slotStart, slotEnd] = extractSlotDates(slot, userTimezone);
+        // IF the slot isn't at least an hour long ignore it
+        if (slot.end.diff(slot.start, 'minutes') < 60) {
+            continue;
+        }
 
-        // If the slot start is at least an hour after the current time
-        // and the end of the slot is after the last free moment
-        // and the slot is at least an hour long 
-        // and the slot is not booked
-        // then mark the slot as available
-        if (slotStart.isSameOrAfter(currentMoment) && lastFreeMoment.isSameOrBefore(slotEnd) && slotEnd.diff(slotStart, 'hours') >= 1) {
-            if (!isSlotBooked(bookedAppointmentTimes, slotStart, slotEnd, userTimezone)) {
-                availableSlots.push([slotStart.toDate(), slotEnd.toDate()]);
+        for (let requestedTime of requestedAppointmentTimes) {
+            if (requestedTime.appointmentTimeStart.length === 4) { // It's a time range
+                // IF the slot start is at least an hour after the current time
+                // AND the slot end is before the last free time
+                // AND the slot is within the range of the requested appointment time
+                // THEN the slot is valid
+                if (slot.start.isSameOrAfter(currentMoment.clone().add(1, 'hours')) &&
+                    slot.end.isSameOrBefore(lastFreeMoment) &&
+                    isSlotWithinRange(slot, requestedTime)) {
+                    results.push([slot.start.toDate(), slot.end.toDate()]);
+                    break;  // Break out of inner loop once a match is found
+                }
+            } else { // It's a specific date time
+                const requestedStart = moment(requestedTime.appointmentTimeStart).tz(userTimezone);
+                const requestedEnd = moment(requestedTime.appointmentTimeEnd).tz(userTimezone);
+
+                // IF the slot start is before the requested appointment start time
+                // AND the slot end is after the requested appoint end time
+                // THEN the slot is valid
+                if (slot.start.isSameOrBefore(requestedStart) &&
+                    slot.end.isSameOrAfter(requestedEnd)) {
+                    results.push([slot.start.toDate(), slot.end.toDate()]);
+                    break;  // Break out of inner loop once a match is found
+                }
             }
         }
     }
 
-    return availableSlots;
+    return results;
 }
 
-// Determines if a slot is booked by comparing the start time 
-const isSlotBooked = (bookedAppointmentTimes: BookedAppointments[], slotStart: moment.Moment, slotEnd: moment.Moment, userTimezone: string): boolean => {
-    for (let appointment of bookedAppointmentTimes) {
-        let appStart = moment.tz(appointment.appointmentTimeStart, userTimezone);
-        let appEnd = moment.tz(appointment.appointmentTimeEnd, userTimezone);
-        if (slotStart.isBefore(appEnd) && slotEnd.isAfter(appStart)) {
-            return true;
-        }
-    }
-    return false;
-};
+export function parseSlot(slot: any, userTimezone: string): Slot {
+    const slotDate = moment(slot.date, 'MM/DD/YYYY').tz(userTimezone);
+    const hyphenSplit = slot.text.split(' - ');
+    const [startHour, startMin] = hyphenSplit[0].split(':');
+    const [endHour, endMin] = hyphenSplit[1].split(':');
+    const slotStartMoment = slotDate.clone().hour(parseInt(startHour)).minute(parseInt(startMin));
+    const slotEndMoment = slotDate.clone().hour(parseInt(endHour)).minute(parseInt(endMin));
 
-// Parse the date and times from a slot
-const extractSlotDates = (slot: any, userTimezone: string): [Moment, Moment] => {
-    let slotDate = moment.tz(slot.date, 'MM/DD/YYYY', userTimezone);
+    return { 
+        start: slotStartMoment, 
+        end: slotEndMoment,
+    };
+}
 
-    const [start, end] = slot.text.split('-').map(t => t.trim());
-    const [startHour, startMinute] = start.split(':').map(Number);
-    const [endHour, endMinute] = end.split(':').map(Number);
+export function isSlotWithinRange(slot: Slot, requestedTime: RequestedAppointment) {
+    const rangeStart = requestedTime.appointmentTimeStart;
+    const rangeEnd = requestedTime.appointmentTimeEnd;
 
-    let slotStart = slotDate.clone().hour(startHour).minute(startMinute);
-    let slotEnd = slotDate.clone().hour(endHour).minute(endMinute);
+    const [startHour, startMinute] = parseHourAndMinute(rangeStart);
+    const [endHour, endMinute] = parseHourAndMinute(rangeEnd);
 
-    return [slotStart, slotEnd];
-};
+    // Determines if the slot is within a range by hours and minutes
+    return (slot.start.hours() > startHour || (slot.start.hours() === startHour && slot.start.minutes() >= startMinute)) &&
+        (slot.end.hours() < endHour || (slot.end.hours() === endHour && slot.end.minutes() <= endMinute));
+}
 
-
+export function parseHourAndMinute(timeStr: string): [number, number] {
+    return [
+        parseInt(timeStr.substring(0, 2)), 
+        parseInt(timeStr.substring(2)),
+    ];
+}
